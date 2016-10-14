6eab83ccbd29a2fe085405a228ee1b7498a8b413
Riber Li <riber.li@hirede.com>
2016-10-14 17:07:23 +0800

    init

---
+++ LieBiDe/new-feature-guide.js
@@ -0,0 +1,29 @@
+function NewFeatureGuide(opts){
+	      this.newFeature=opts.newFeature;
+      this.$el=$(opts.tpl);
+      this.$guideBox=this.$el.find(".feature-guide-box");
+}
+
+NewFeatureGuide.prototype.showFeature=function(){
+       let $featureBox=$(this.newFeature),
+           $featureClone=$featureBox.clone();
+       $featureClone.width($featureBox.width())
+                   .height($featureBox.height());
+       $featureClone.appendTo(this.$guideBox);
+
+       this.$el.show();
+       var offset=$featureBox.offset;
+       this.$guideBox.css({
+       	left:offset.left,
+       	top:offset.top
+       })
+
+};
+
+$(function(){
+	var newFeatureGuide=new NewFeatureGuide({
+		newFeature:".luck-money-broadcast",
+        tpl:".new-feature-guide"
+	})
+	newFeatureGuide.showFeature();
+});
